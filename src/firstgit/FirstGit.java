136e5ff3abd7808791e22726ffa8550da2b49ac0
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-08-28 21:32:36 -0500

    Added Delete Line

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -3,5 +3,6 @@ package firstgit;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("To Be Deleted");
     }
 }
