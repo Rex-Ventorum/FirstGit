8ff3af740acd48ea3f0bc50bf4733344aceff229
L117student <L117student@L117-18-043663.bitnet.wctc.edu>
2017-08-28 21:33:00 -0500

    Revert "Added a dummy comment because I already removed the first ones :P"
    This reverts commit 87c00848cd89e7e8f941de06394b1fcba0ad112b.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,8 +1,10 @@
 package firstgit;
-//Adding a comment 
+
 public class FirstGit {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("To Be Deleted");
     }
+    
 }
